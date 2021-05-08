f5a882b0804d042e4432412a8002d65aef4b2af1
Elysia Yong <elysiayong@gmail.com>
2021-05-08 23:02:09 +0800

    Bugfix word wrapping

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,12 +4,14 @@ const Item = ({ id, item, important, quantity, category, onDelete, onToggle}) =>
     return (
         <div className={`item ${important ? 'important' : ''}`} 
         onDoubleClick={ () => onToggle(id)}>
-            <h3> 
-                {item} 
+            <span>
+                <h3> 
+                    {item} 
+                </h3>
                 <FaTimes 
-                    style={{ color: 'red', cursor: 'pointer'}} 
-                    onClick={ () => onDelete(id)}/>
-            </h3>
+                        style={{ color: 'red', cursor: 'pointer'}} 
+                        onClick={ () => onDelete(id)}/>
+            </span>
             { category !== 'None' ? <p> {category} </p> : ''}
             <p> {"Amount: " + quantity} </p>
         </div>
